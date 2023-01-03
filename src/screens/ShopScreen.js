fbf94a74c4454016ad912ac54bb7840e5564b042
KawshikBss <kawshikbss@gmail.com>
2023-01-03 18:48:04 +0600

    added shop bannner and shop sections

--- src/screens/ShopScreen.js
+++ src/screens/ShopScreen.js
@@ -1,14 +1,25 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet, ScrollView } from "react-native";
+import ShopBanner from "../components/Banners/ShopBanner";
 import Navbar from "../components/Navbar";
+import ShopSection from "../components/Shop/ShopSection";
 import { getRouteName } from "../Services/Utilities";
 
-const ShopScreen = ({navigation}) => {
-    return (
-        <View>
-        <Text>Shop Screen</Text>
-        <Navbar currentTab={getRouteName(navigation)} />
-        </View>
-    );
+const ShopScreen = ({ navigation }) => {
+  return (
+    <View>
+      <ScrollView style={styles.container}>
+        <ShopBanner />
+        <ShopSection />
+      </ScrollView>
+      <Navbar navigation={navigation} currentTab={"Shop"} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: 470,
+  },
+});
 
 export default ShopScreen;
