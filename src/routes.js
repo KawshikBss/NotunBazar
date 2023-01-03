87631de1a1feaf705255bd08fbd450b3436adb6d
KawshikBss <kawshikbss@gmail.com>
2023-01-03 15:36:11 +0600

    to store routes

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import { lazy } from "react";
+
+const routes = [
+    {
+        name: 'Home',
+        icon: lazy(() => import('react-native-vector-icons/FontAwesome')),
+        iconName: 'home'
+    },
+    {
+        name: 'Shop',
+        icon: lazy(() => import('react-native-vector-icons/FontAwesome')),
+        iconName: 'shopping-cart'
+    },
+    {
+        name: 'Cart',
+        icon: lazy(() => import('react-native-vector-icons/FontAwesome')),
+        iconName: 'shopping-bag'
+    },
+    {
+        name: 'Settings',
+        icon: lazy(() => import('react-native-vector-icons/FontAwesome')),
+        iconName: 'user'
+    }
+];
+
+export default routes;
