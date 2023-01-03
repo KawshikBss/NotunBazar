af9b3eede81f1973747fe1c6cb98e0d1d2565f5e
KawshikBss <kawshikbss@gmail.com>
2023-01-03 20:45:31 +0600

    added cards

--- src/components/Shop/ShopSection.js
+++ src/components/Shop/ShopSection.js
@@ -1,48 +1,68 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import products from "../../products";
+import Card from "./Card";
 
-const ShopSection = ({header, subHeader, next}) => {
-    return (
-        <View style={styles.container}>
-            <View style={styles.wrapper}>
-                <View style={styles.headerContainer}>
-                    <Text style={styles.header}>{header? header: 'Header'}</Text>
-                    <Text style={styles.subHeader}>{subHeader? subHeader: 'Sub Header'}</Text>
-                </View>
-                <TouchableOpacity style={styles.nextBtn}>
-                    <Text style={styles.nextText}>View All</Text>
-                </TouchableOpacity>
-            </View>
+const ShopSection = ({ header, subHeader, next, items }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.wrapper}>
+        <View style={styles.headerContainer}>
+          <Text style={styles.header}>{header ? header : "Header"}</Text>
+          <Text style={styles.subHeader}>
+            {subHeader ? subHeader : "Sub Header"}
+          </Text>
         </View>
-    );
+        <TouchableOpacity style={styles.nextBtn}>
+          <Text style={styles.nextText}>View All</Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView horizontal style={styles.itemsContainer}>
+        {
+            items?
+            items.map((item) => {
+                return (<Card key={item.id} product={item} />)
+            }):
+            <Text>No Products</Text>
+        }
+      </ScrollView>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'center'
-    },
-    wrapper: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingHorizontal: 10,
-        marginVertical: 20
-    },
-    headerContainer: {
-
-    },
-    header: {
-        color: 'black',
-        fontSize: 40,
-    },
-    subHeader: {
-        color: 'gray',
-        fontSize: 18,
-    },
-    nextBtn: {
-
-    },
-    nextText : {
-        fontSize: 16
-    }
+  container: {
+    marginBottom: 200,
+  },
+  wrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    marginVertical: 20,
+  },
+  headerContainer: {},
+  header: {
+    color: "black",
+    fontSize: 40,
+  },
+  subHeader: {
+    color: "gray",
+    fontSize: 18,
+  },
+  itemsContainer: {
+    height: 320,
+  },
+  nextBtn: {},
+  nextText: {
+    fontSize: 16,
+  },
 });
 
 export default ShopSection;
