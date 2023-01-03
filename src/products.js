b75ab9887e1356df64079977aa8cba4bdcb35d37
KawshikBss <kawshikbss@gmail.com>
2023-01-03 20:33:56 +0600

    for dummy products

---
+++ src/products.js
@@ -0,0 +1,53 @@
+const products = [
+    {
+        id: 0,
+        title: 'Aliquip',
+        description: 'Esse adipisicing tempor occaecat magna nulla culpa consectetur. Incididunt ad incididunt nisi Lorem enim deserunt Lorem nostrud quis excepteur laborum cupidatat qui Lorem. Occaecat enim consectetur eiusmod nostrud sint velit Lorem Lorem aute. Nostrud tempor Lorem dolor fugiat laborum amet mollit consectetur id nisi. Laboris consequat qui sint non ipsum qui quis. Et incididunt magna voluptate sit exercitation consequat ea amet labore do deserunt id. In minim fugiat esse minim ea velit sint veniam voluptate reprehenderit occaecat.',
+        price: 100,
+        discount: 20
+    },
+    {
+        id: 1,
+        title: 'Exercitation',
+        description: 'Esse adipisicing tempor occaecat magna nulla culpa consectetur. Incididunt ad incididunt nisi Lorem enim deserunt Lorem nostrud quis excepteur laborum cupidatat qui Lorem. Occaecat enim consectetur eiusmod nostrud sint velit Lorem Lorem aute. Nostrud tempor Lorem dolor fugiat laborum amet mollit consectetur id nisi. Laboris consequat qui sint non ipsum qui quis. Et incididunt magna voluptate sit exercitation consequat ea amet labore do deserunt id. In minim fugiat esse minim ea velit sint veniam voluptate reprehenderit occaecat.',
+        price: 130,
+        discount: 20
+    },
+    {
+        id: 2,
+        title: 'Eiusmod',
+        description: 'Esse adipisicing tempor occaecat magna nulla culpa consectetur. Incididunt ad incididunt nisi Lorem enim deserunt Lorem nostrud quis excepteur laborum cupidatat qui Lorem. Occaecat enim consectetur eiusmod nostrud sint velit Lorem Lorem aute. Nostrud tempor Lorem dolor fugiat laborum amet mollit consectetur id nisi. Laboris consequat qui sint non ipsum qui quis. Et incididunt magna voluptate sit exercitation consequat ea amet labore do deserunt id. In minim fugiat esse minim ea velit sint veniam voluptate reprehenderit occaecat.',
+        price: 50,
+        discount: 20
+    },
+    {
+        id: 3,
+        title: 'Veniam',
+        description: 'Esse adipisicing tempor occaecat magna nulla culpa consectetur. Incididunt ad incididunt nisi Lorem enim deserunt Lorem nostrud quis excepteur laborum cupidatat qui Lorem. Occaecat enim consectetur eiusmod nostrud sint velit Lorem Lorem aute. Nostrud tempor Lorem dolor fugiat laborum amet mollit consectetur id nisi. Laboris consequat qui sint non ipsum qui quis. Et incididunt magna voluptate sit exercitation consequat ea amet labore do deserunt id. In minim fugiat esse minim ea velit sint veniam voluptate reprehenderit occaecat.',
+        price: 190,
+        discount: 20
+    },
+    {
+        id: 4,
+        title: 'Est minim',
+        description: 'Esse adipisicing tempor occaecat magna nulla culpa consectetur. Incididunt ad incididunt nisi Lorem enim deserunt Lorem nostrud quis excepteur laborum cupidatat qui Lorem. Occaecat enim consectetur eiusmod nostrud sint velit Lorem Lorem aute. Nostrud tempor Lorem dolor fugiat laborum amet mollit consectetur id nisi. Laboris consequat qui sint non ipsum qui quis. Et incididunt magna voluptate sit exercitation consequat ea amet labore do deserunt id. In minim fugiat esse minim ea velit sint veniam voluptate reprehenderit occaecat.',
+        price: 200,
+        discount: 20
+    },
+    {
+        id: 5,
+        title: 'Ea ullamco',
+        description: 'Esse adipisicing tempor occaecat magna nulla culpa consectetur. Incididunt ad incididunt nisi Lorem enim deserunt Lorem nostrud quis excepteur laborum cupidatat qui Lorem. Occaecat enim consectetur eiusmod nostrud sint velit Lorem Lorem aute. Nostrud tempor Lorem dolor fugiat laborum amet mollit consectetur id nisi. Laboris consequat qui sint non ipsum qui quis. Et incididunt magna voluptate sit exercitation consequat ea amet labore do deserunt id. In minim fugiat esse minim ea velit sint veniam voluptate reprehenderit occaecat.',
+        price: 105,
+        discount: 20
+    },
+    {
+        id: 6,
+        title: 'Commodo',
+        description: 'Esse adipisicing tempor occaecat magna nulla culpa consectetur. Incididunt ad incididunt nisi Lorem enim deserunt Lorem nostrud quis excepteur laborum cupidatat qui Lorem. Occaecat enim consectetur eiusmod nostrud sint velit Lorem Lorem aute. Nostrud tempor Lorem dolor fugiat laborum amet mollit consectetur id nisi. Laboris consequat qui sint non ipsum qui quis. Et incididunt magna voluptate sit exercitation consequat ea amet labore do deserunt id. In minim fugiat esse minim ea velit sint veniam voluptate reprehenderit occaecat.',
+        price: 40,
+        discount: 20
+    },
+];
+
+export default products;
