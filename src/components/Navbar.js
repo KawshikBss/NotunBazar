88663f67dba5cc8c335148be7e79b52193752d7d
KawshikBss <kawshikbss@gmail.com>
2023-01-03 16:00:24 +0600

    added navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,25 +7,30 @@ import {
   View,
 } from "react-native";
 import routes from "../routes";
+import { getRouteName } from "../Services/Utilities";
 
-const Navbar = ({currentTab}) => {
-    const [activeNav, setActiveNav] = useState(currentTab? currentTab: 'Home');
-    const getNavStatus = (navName) => {
-        return navName === activeNav? styles.navActive: styles.navIcon;
-    }
-    const changeNav = (navName) => {
-        setActiveNav(currNav => {
-            if (navName === currNav) return currNav;
-            return navName;
-        })
-    }
+const Navbar = ({ navigation, currentTab = 'Home' }) => {
+  const [activeNav, setActiveNav] = useState(currentTab);
+  const getNavStatus = (navName) => {
+    return navName === activeNav ? styles.navActive : styles.navIcon;
+  };
+  const changeNav = (navName) => {
+    if (navName === activeNav || navName.length === 0) return;
+    setActiveNav(navName);
+  };
   return (
     <View style={styles.container}>
       <FlatList
         keyExtractor={(item) => item.name}
         data={routes}
         renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => changeNav(item.name)} style={styles.navBtn}>
+          <TouchableOpacity
+            onPress={() => {
+              changeNav(item.name);
+              navigation.navigate(item.name);
+            }}
+            style={styles.navBtn}
+          >
             <item.icon style={getNavStatus(item.name)} name={item.iconName} />
           </TouchableOpacity>
         )}
@@ -36,32 +41,32 @@ const Navbar = ({currentTab}) => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: "row",
-        position: "absolute",
-        bottom: -40,
-        left: 0,
-        right: 0,
-        backgroundColor: '#FFF',
-        height: 50,
-        alignItems: "center",
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10,
-    },
-    navBtn: {
-        flex: 1,
-        alignItems: 'center',
-    },
-    navIcon: {
-        fontSize: 25,
-        color: '#700D05',
-        opacity: 0.5,
-    },
-    navActive: {
-        fontSize: 25,
-        color: '#DB3022',
-        opacity: 1,
-    }
+  container: {
+    flexDirection: "row",
+    position: "absolute",
+    bottom: -40,
+    left: 0,
+    right: 0,
+    backgroundColor: "#FFF",
+    height: 50,
+    alignItems: "center",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  navBtn: {
+    flex: 1,
+    alignItems: "center",
+  },
+  navIcon: {
+    fontSize: 25,
+    color: "#700D05",
+    opacity: 0.5,
+  },
+  navActive: {
+    fontSize: 25,
+    color: "#DB3022",
+    opacity: 1,
+  },
 });
 
 export default Navbar;
