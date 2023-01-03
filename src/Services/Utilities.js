38892a95837e9137b9c459bba990c075af86cd27
KawshikBss <kawshikbss@gmail.com>
2023-01-03 18:48:22 +0600

    removed log

--- src/Services/Utilities.js
+++ src/Services/Utilities.js
@@ -1,5 +1,5 @@
 export const getRouteName = (navigation) => {
     const route = navigation?.getState()?.routes[0]?.name? navigation.getState().routes[0].name: '';
-    console.log(route);
+    //console.log(route);
     return route;
 }
