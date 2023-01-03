dc36970aed895dac922fbbd266246f7cf5baa62b
KawshikBss <kawshikbss@gmail.com>
2023-01-03 21:13:30 +0600

    added sections

--- src/screens/ShopScreen.js
+++ src/screens/ShopScreen.js
@@ -3,13 +3,15 @@ import ShopBanner from "../components/Banners/ShopBanner";
 import Navbar from "../components/Navbar";
 import ShopSection from "../components/Shop/ShopSection";
 import { getRouteName } from "../Services/Utilities";
+import products from "../products";
 
 const ShopScreen = ({ navigation }) => {
   return (
     <View>
       <ScrollView style={styles.container}>
         <ShopBanner />
-        <ShopSection />
+        <ShopSection header={'Sale'} subHeader={'Winter Sale'} items={products.sale} />
+        <ShopSection header={'New'} subHeader={'New Collections'} items={products.new} />
       </ScrollView>
       <Navbar navigation={navigation} currentTab={"Shop"} />
     </View>
