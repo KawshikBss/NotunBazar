b7cf3bb421be27c85ee7fbab5af041a0fc447011
KawshikBss <kawshikbss@gmail.com>
2023-01-04 00:55:38 +0600

    added new screeens

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { FlatList, StyleSheet, Text, View } from "react-native";
+import CartScreen from "./src/screens/CartScreen";
 import HomeScreen from "./src/screens/HomeScreen";
+import ProductScreen from "./src/screens/ProductScreen";
 import ShopScreen from "./src/screens/ShopScreen";
+import SplashScreen from "./src/screens/SplashScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,8 +13,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Wellcome" component={SplashScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Shop" component={ShopScreen} />
+        <Stack.Screen name="Cart" component={CartScreen} />
+        <Stack.Screen name="Product" component={ProductScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
