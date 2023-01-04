d4317a667859e4ecb4e23a608d6d62d99c920b4a
KawshikBss <kawshikbss@gmail.com>
2023-01-04 22:58:38 +0600

    created profile screen

---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,25 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+const ProfileScreen = () => {
+    return (
+        <View>
+            <Text>ProfileScreen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        height: 470,
+        backgroundColor: "#E9E9E9",
+    },
+    heading: {},
+    header: {},
+    image: {},
+    infoWrapper: {},
+    name: {},
+    infoText: {},
+});
+
+export default ProfileScreen;
